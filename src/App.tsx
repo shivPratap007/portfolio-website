bf9a966303af79797a01bf1dff6f5596ef2f60f8
Shiv Pratap <sp09singhwaghel@gmail.com>
2024-08-10 17:09:52 +0530

    Add: nav-bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import Hero from "./components/Hero"
+import Navbar from "./components/Navbar"
 
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
       <div className="fixed inset-0 bg-fixed bg-cover bg-center"></div>
       <div className="relative z-10 flex flex-col items-cneter p-4 space-y-8 container mx-auto">
         <Hero/>
+        <Navbar/>
       </div>
     </div>
     </>
